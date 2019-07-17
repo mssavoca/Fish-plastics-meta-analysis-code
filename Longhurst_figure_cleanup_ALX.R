90009dd105729a72618f4a034c7d3c27d3d7e413
Alex <agmcinturf@gmail.com>
2019-07-17 13:09:23 +0100

    Fixing Longhurst Map fig

--- Longhurst_figure_cleanup_ALX.R
+++ Longhurst_figure_cleanup_ALX.R
@@ -1,4 +1,14 @@
 ## Alex prepping data for longhurst province figure ##
+# load packages and data ----
+library(tidyverse)
+library(gbm)
+library(dismo)
+library(mgcv)
+library(lme4)
+library(gamm4)
+library(readxl)
+library(readr)
+library(ggplot2)
 
 data <- read.csv("Plastics ingestion records fish master_UDPATED_AGM-MSS2.csv")
 
@@ -6,9 +16,9 @@ data <- read.csv("Plastics ingestion records fish master_UDPATED_AGM-MSS2.csv")
 summary(data)
 head(data)
 # data of interest
-data2 <- data[,c("Oceanographic.province..from.Longhurst.2007.", "Prop.w.plastic", "NwP", "N", "Source")]
+data2 <- data[,c("Species.name", "Oceanographic.province..from.Longhurst.2007.", "Prop.w.plastic", "NwP", "N", "Source")]
 head(data2)
-colnames(data2) <- c("OceanProv", "PropPlastic", "NwP", "N", "Source")
+colnames(data2) <- c("Species", "OceanProv", "PropPlastic", "NwP", "N", "Source")
 head(data2)
 length(table(data2$OceanProv))
 data3<- data2[order(data2$OceanProv),]
@@ -23,16 +33,24 @@ length(prov)
 aveplast <- rep(NA, length(prov))
 numfish <- rep(NA, length(prov))
 numstudies <- rep(NA, length(prov))
+numspecies <- rep(NA, length(prov))
+normalized <- rep(NA, lenght(prov))
 
 ## using this to double check the produced averages
-sub <- data3[data3$OceanProv=="AUSW",]
+sub <- data3[data3$OceanProv=="CHIL",]
 sub
 unique(sub$Source)
 length(unique(sub$Source))
-sum(sub$NwP)/sum(sub$N)
+overallprop <- sum(sub$NwP)/sum(sub$N)
+overallprop
+normalized <- overallprop*sum(sub$N)
+normalized
+sum(sub$N)
 mean(sub$PropPlastic) #3 = .1077803
 sum(sub$N)
 nrow(sub)
+length(unique(sub$Species))
+
 
 
 for (i in 1:length(prov)){
@@ -40,6 +58,8 @@ for (i in 1:length(prov)){
   aveplast[i] <- sum(sub$NwP)/sum(sub$N)
   numfish[i] <- sum(sub$N)
   numstudies[i] <- length(unique(sub$Source))
+  numspecies[i] <- length(unique(sub$Species))
+  normalized[i] <- aveplast[i]*sum(sub$N)
 }
 
 aveplast
@@ -47,9 +67,11 @@ length(aveplast)
 numfish
 table(numfish)
 numstudies
+head(data3)
+numspecies
 
 
-newdat <- data.frame(prov, aveplast, numfish, numstudies)
+newdat <- data.frame(prov, aveplast, numfish, numstudies, numspecies, normalized)
 newdat
 
 ## binning data
@@ -92,15 +114,39 @@ newdat$numfishbin[newdat$numfish >1500]= ">1500"
 
 head(newdat, 50)
 
-head(newdat)
+# normalized proportion of ingestion bins
+summary(newdat$normalized)
+newdat$normbin <- rep(NA, length(prov))
+newdat$normbin[newdat$normalized >=1 & newdat$normalized<25]= "< 10"
+newdat$normbin[newdat$normalized >=10 & newdat$normalized<=50]= "10-50"
+newdat$normbin[newdat$normalized >50 & newdat$normalized<=100]= "51-100"
+newdat$normbin[newdat$normalized >100 & newdat$normalized<500]= "101-500"
+newdat$normbin[newdat$normalized >500 & newdat$normalized<=1000]= "501-1000"
+newdat$normbin[newdat$normalized >1000 & newdat$normalized<=1500]= "1001-1500"
+newdat$normbin[newdat$normalized >1500]= ">1500"
 
-table(newdat$prov)
 
-head(newdat)
+
+
+head(newdat, 50)
 newdat$aveplast
 sort(newdat$numstudies)
 sort(newdat$numfish)
 write.csv(newdat, "Longhurst_FishSummaryData_fullbinned.csv")
 
 
+newdat
 
+### Summary table:
+# geographic summary of data
+Geo_summ_pt2 <- newdat %>% 
+  group_by(`prov`) %>% 
+  summarize(num_studies = numstudies,
+            num_sp = numspecies,
+            num_ind_studied = numfish)
+
+Geo_summ_pt2
+nrow(Geo_summ_pt2)
+ncol(Geo_summ_pt2)
+
+write.table(Geo_summ_pt2, file = "Geo_summ_pt2.txt", sep = ",", quote = FALSE, row.names = F)
